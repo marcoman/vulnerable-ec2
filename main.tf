82567837c4fcaf538f800b785e7c98443417c5de
Marco Morales <mr.marco.a.morales@gmail.com>
2022-10-19 09:13:44 -0400

    update CIDR to a specific IP address for egress

--- main.tf
+++ main.tf
@@ -39,7 +39,8 @@ resource "aws_security_group" "allow_ssh_from_anywhere" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["68.80.18.164/32"]
+#    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
